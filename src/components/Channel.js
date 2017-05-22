411a1f916f847cc7d29955cbd0d78f2640f42f82
Mars Cheung <marscheung@Marss-MacBook-Pro.local>
2017-05-22 13:22:47 -0700

    add basic websocket to channel component and succesfully establish connection

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,45 +1,67 @@
 import Message from './Message';
 import React from 'react';
 import ChatInput from './ChatInput';
+// import io from 'socket.io-client';
+import config from '../config/config';
 import './channel.css'
 
 class Channel extends React.Component{
 	
-	constructor(){
-		super();
+	socket = {};
+	constructor(props){
+		super(props);
 		this.state = {
 			messages:[]
-		}
+		}    // Connect to the server
+		this.clientInformation = {
+        	authToken: props.authToken
+        	// You can add more information in a static object
+    	};
+    	this.conn = new WebSocket('ws://localhost:8090');
+
+		this.conn.onopen = function(e) {
+			console.info("Connection established succesfully");
+		};
+    	// this.socket = io(config.api).connect();
+
+	    // // Listen for messages from the server
+	    // this.socket.on('server:message', message => {
+	    //   this.addMessage(message);
+	    // });
 	}
 
 	componentWillMount(){
-		var url = 'http://localhost:8000/messages/getMessagesInChannel';
-		return fetch(url, {
-		  method: 'POST',
-		  headers: {
-		    'Authorization': this.props.authToken,
-		    'Content-Type': 'application/json'
-		  },
-		  body: JSON.stringify({
-		    channel_id: this.props.index
-		  })
-		})
-		.then((response) => response.json())
-		.then((responseJson) => {
-			this.setState({
-				messages:responseJson.messages
-			});
-      	})
-		.catch((error) => {
-			console.log(error);
-			console.error(error);
-		});
+
+		this.conn.onmessage = function (event) {
+		  console.log(event.data);
+		}
+		// var url = 'http://localhost:8000/messages/getMessagesInChannel';
+		// return fetch(url, {
+		//   method: 'POST',
+		//   headers: {
+		//     'Authorization': this.props.authToken,
+		//     'Content-Type': 'application/json'
+		//   },
+		//   body: JSON.stringify({
+		//     channel_id: this.props.index
+		//   })
+		// })
+		// .then((response) => response.json())
+		// .then((responseJson) => {
+		// 	this.setState({
+		// 		messages:responseJson.messages
+		// 	});
+  //     	})
+		// .catch((error) => {
+		// 	console.log(error);
+		// 	console.error(error);
+		// });
 	}
 
-	componentDidUpdate() {
-		const objDiv = document.getElementById('transcript');
-		objDiv.scrollTop = objDiv.scrollHeight;
-	}
+	// componentDidUpdate() {
+	// 	const objDiv = document.getElementById('transcript');
+	// 	objDiv.scrollTop = objDiv.scrollHeight;
+	// }
 
 	render(){
 		const {details, index} = this.props;
