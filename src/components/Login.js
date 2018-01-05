9e334405b657fe743b7f8770792645dd8c752920
Mars Cheung <mars.kwong.cheung@gmail.com>
2018-01-04 23:46:54 -0800

    add new user registration modal

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'whatwg-fetch';
 import { Route, Redirect }  from 'react-router';
+import Modal from 'react-responsive-modal';
 import ConversationPanel from './ConversationPanel';
 
 const renderMergedProps = (component, ...rest) => {
@@ -20,7 +21,13 @@ const PropsRoute = ({ component, ...rest }) => {
 class Login extends React.Component {
 	constructor(){
 		super();
+		this.state={
+			open: false,
+		}
 		this.handleSubmit=this.handleSubmit.bind(this);
+		this.handleRegisterSubmit=this.handleRegisterSubmit.bind(this);
+		this.onOpenModal=this.onOpenModal.bind(this);
+		this.onCloseModal=this.onCloseModal.bind(this);
 	}
 
 	componentWillMount(){
@@ -29,7 +36,16 @@ class Login extends React.Component {
 		}
 	}
 
+	onOpenModal(){
+    	this.setState({ open: true });
+  	};
+ 
+	onCloseModal(){
+		this.setState({ open: false });
+	};
+
 	render(){
+		const { open } = this.state;
 		return(
 				<div className="container-fluid">
 					<div className="row">
@@ -46,6 +62,28 @@ class Login extends React.Component {
 							</div>
 						</form>
 					</div>
+					<button onClick={this.onOpenModal}>Open modal</button>
+					<Modal open={open} onClose={this.onCloseModal} little>
+						<h2>New User Registration</h2>
+						<form onSubmit={this.handleRegisterSubmit}>
+							<br/>
+							<div className="form-group">
+								<label>First Name</label>
+								<input name="firstName" type="text" />
+								<br/>
+								<label>Last Name</label>
+								<input name="lastName" type="text" />
+								<br/>
+								<label>Email</label>
+								<input name="email" type="text" />
+								<br/>
+								<label>Password</label>
+								<input type="password" name="password" />
+								<br/>
+								<input type="submit" className="btn btn-primary"/>
+							</div>
+						</form>
+					</Modal>
 				</div>
 		);
 	}
@@ -55,7 +93,7 @@ class Login extends React.Component {
 		var email = event.target.email.value;
 		var password = event.target.password.value;
 		var url = 'http://localhost:3000/token';
-
+console.log(this);
 		return fetch(url, {
 		  method: 'POST',
 		  headers: {
@@ -75,6 +113,40 @@ class Login extends React.Component {
 		.catch((error) => {
 			console.log(error);
 			console.error(error);
+		});
+	}
+
+	handleRegisterSubmit(event){
+		event.preventDefault();
+		console.log(this);
+		var self = this;
+		var firstName = event.target.firstName.value;
+		var lastName = event.target.lastName.value;
+		var email = event.target.email.value;
+		var password = event.target.password.value;
+		var url = 'http://localhost:3000/users';
+		console.log(event);
+		return fetch(url, {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+					firstName: firstName,
+					lastName: lastName,
+					email: email,
+					password: password
+				})
+			})
+			.then((response) => response.json())
+			.then((responseJson) => {
+				console.log(responseJson);
+				this.onCloseModal();
+			})
+			.catch((error) => {
+				console.log(error);
+				console.error(error);
 		});
 	}
 }
