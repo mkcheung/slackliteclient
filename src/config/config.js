07fd103c477f1e97f66308a08bba02a14d156fc5
Mars Cheung <mars.kwong.cheung@gmail.com>
2018-01-21 23:03:53 -0800

    modify for deployment master

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 'use strict';
 
 // Settings configured here will be merged into the final config object.
-export const chatServerDomain = 'http://localhost:3000/'
+export const chatServerDomain = 'https://multislacklite-jnsgpmouhh.now.sh/'
