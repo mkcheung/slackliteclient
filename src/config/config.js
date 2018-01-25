2d0f53581f0c870bb6b082f78a48c797c15f2e53
Mars Cheung <mars.kwong.cheung@gmail.com>
2018-01-24 21:48:10 -0800

    correct typo in config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 'use strict';
 
 // Settings configured here will be merged into the final config object.
-export const chatServerDomain = 'https://multislacklite-lwtbdczdhk.now.sh'
+export const chatServerDomain = 'https://multislacklite-lwtbdczdhk.now.sh/'
