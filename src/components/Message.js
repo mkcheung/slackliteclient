4e31b8b540c1f9fdfc7c9a61102fc86f1b39887e
Mars Cheung <mars.kwong.cheung@gmail.com>
2018-01-28 21:49:14 -0800

    use react-moment to adjust and display times of message creation

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Moment from 'react-moment';
 
 class Message extends React.Component{
 	render(){
 		return(
 			<li>
-				<span>{this.props.details.user.email} {this.props.details.created}</span>
+				<span>{this.props.details.user.email} <Moment format="MM-DD-YYYY hh:mm a">{this.props.details.created}</Moment></span>
 				<br/>
 				<span>{this.props.details.message}</span>
 			</li>
