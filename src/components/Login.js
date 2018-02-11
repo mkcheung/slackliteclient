e7f5c47d437e811439e19137efe22bef98226ca1
Mars Cheung <mars.kwong.cheung@gmail.com>
2018-02-11 00:23:36 -0800

    redesign and style login page with reactstrap and bootstrap 4

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,7 @@ import Modal from 'react-responsive-modal';
 import ConversationPanel from './ConversationPanel';
 import 'react-responsive-modal/lib/react-responsive-modal.css';
 import * as configConsts from '../config/config';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 var NotificationSystem = require('react-notification-system');
 
 const renderMergedProps = (component, ...rest) => {
@@ -26,12 +27,14 @@ class Login extends React.Component {
 		super();
 		this.state={
 			open: false,
+			isOpen: false
 		}
 		this.notificationSystem= null,
 		this.handleSubmit=this.handleSubmit.bind(this);
 		this.handleRegisterSubmit=this.handleRegisterSubmit.bind(this);
 		this.onOpenModal=this.onOpenModal.bind(this);
 		this.onCloseModal=this.onCloseModal.bind(this);
+		this.toggle = this.toggle.bind(this);
 	}
 
 	componentWillMount(){
@@ -52,48 +55,61 @@ class Login extends React.Component {
 		this.setState({ open: false });
 	};
 
+	toggle() {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	}
+
 	render(){
 		const { open } = this.state;
 		return(
-				<div className="container-fluid">
-					<div className="row">
-						<form onSubmit={this.handleSubmit}>
-							<br/>
-							<div className="form-group">
-								<label>Email</label>
-								<input name="email" type="text" />
-								<br/>
-								<label>Password</label>
-								<input type="password" name="password" />
-								<br/>
-								<input type="submit" className="btn btn-primary"/>
-							</div>
-						</form>
-					</div>
-					<button onClick={this.onOpenModal}>Register User</button>
-					<Modal open={open} onClose={this.onCloseModal} little>
-						<h2>New User Registration</h2>
-						<form onSubmit={this.handleRegisterSubmit}>
-							<br/>
-							<div className="form-group">
-								<label>First Name</label>
-								<input name="firstName" type="text" />
-								<br/>
-								<label>Last Name</label>
-								<input name="lastName" type="text" />
-								<br/>
-								<label>Email</label>
-								<input name="email" type="text" />
-								<br/>
-								<label>Password</label>
-								<input type="password" name="password" />
-								<br/>
-								<input type="submit" className="btn btn-primary"/>
-							</div>
-						</form>
-					</Modal>
-			        <NotificationSystem ref="notificationSystem" />
-				</div>
+			<div className="container-fluid">
+				<Navbar color="faded" light expand="md">
+				<NavbarBrand href="/">MultiChat</NavbarBrand>
+				<NavbarToggler onClick={this.toggle} />
+					<Collapse isOpen={this.state.isOpen} navbar>
+						<Nav className="ml-auto" navbar>
+ 							<Form inline onSubmit={this.handleSubmit}>
+								<FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+									<Label className="mr-sm-2">Email:</Label>
+									<Input name="email" type="text" />
+								</FormGroup>
+								<FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+									<Label className="mr-sm-2">Password:</Label>
+									<Input type="password" name="password" />
+								</FormGroup>
+								<FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+									<Button color="primary" className="btn btn-primary mr-sm-2">Submit</Button>
+									<Button color="primary" onClick={this.onOpenModal}>Register User</Button>
+								</FormGroup>
+							</Form>
+							<Modal open={open} onClose={this.onCloseModal} little>
+							<h2>New User Registration</h2>
+								<form onSubmit={this.handleRegisterSubmit}>
+									<br/>
+									<div className="form-group">
+										<label>First Name</label>
+										<input name="firstName" type="text" />
+										<br/>
+										<label>Last Name</label>
+										<input name="lastName" type="text" />
+										<br/>
+										<label>Email</label>
+										<input name="email" type="text" />
+										<br/>
+										<label>Password</label>
+										<input type="password" name="password" />
+										<br/>
+										<input type="submit" className="btn btn-primary"/>
+									</div>
+								</form>
+							</Modal>
+							<NotificationSystem ref="notificationSystem" />
+						</Nav>
+					</Collapse>
+				</Navbar>
+			</div>
 		);
 	}
 
